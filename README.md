96311d2c09838a1ed8647b448616aef2aa376491
dastagg:opus5 <dastagg@stagglinux.com>
2022-04-09 07:26:34 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# cubit2cubit
+
+A Flutter tutorial application by Your Dev Edge.
+
+## Description
+
+This is a series of tutorials that focuses on State and State Management using the Bloc Package.
+
+This is one of several "Overview" applications that demonstrate the basics of Cubit/Bloc.
+
+This app will demonstrate Cubit to Cubit communication using StreamSubscription.
---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'cubit2cubit',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: const Text(
+                'Change Color',
+                style: TextStyle(fontSize: 24.0),
+              ),
+              onPressed: () {},
+            ),
+            const SizedBox(height: 20.0),
+            const Text(
+              '0',
+              style: TextStyle(
+                fontSize: 52.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            ElevatedButton(
+              child: const Text(
+                'Increment Counter',
+                style: TextStyle(fontSize: 24.0),
+              ),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
